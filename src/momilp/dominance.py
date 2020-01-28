e7b4b941df1aafbde91fa6122a5f7f3bdc3fa1e0
gokhanceyhan <gokhanceyhan01@gmail.com>
2020-01-29 00:44:41 +0300

    completed the rules and the tests for the dominated conditions

--- src/momilp/dominance.py
+++ src/momilp/dominance.py
@@ -12,6 +12,36 @@ class DominanceRules:
 
     """Implements rules to identify the dominance relations between two sets of points, edges or frontiers"""
 
+    class EdgeToEdge:
+
+        """Implements the rules when comparing an edge to another edge"""
+
+        @staticmethod
+        def dominated(this, that):
+            """Returns True if 'this' edge is dominated by 'that' edge, otherwise False"""
+            assert isinstance(this, EdgeInTwoDimension) and isinstance(that, EdgeInTwoDimension), "the edges must be " \
+                "in the two-dimensional space"
+            if not DominanceRules.PointToEdge.dominated(this.left_point(), that):
+                return False
+            if not DominanceRules.PointToEdge.dominated(this.right_point(), that):
+                return False            
+            return True
+
+    class EdgeToFrontier:
+
+        """Implements the rules when comparing an edge to a frontier"""
+
+        @staticmethod
+        def dominated(edge, frontier):
+            """Returns True if the edge is dominated by the frontier, otherwise False"""
+            assert isinstance(edge, EdgeInTwoDimension) and isinstance(frontier, FrontierInTwoDimension), "the edge " \
+                "and the frontier must be in the two-dimensional space"
+            if not DominanceRules.PointToFrontier.dominated(edge.left_point(), frontier):
+                return False
+            if not DominanceRules.PointToFrontier.dominated(edge.right_point(), frontier):
+                return False            
+            return True
+    
     class EdgeToPoint:
 
         """Implements the rules when comparing an edge to a point"""
@@ -27,6 +57,38 @@ class DominanceRules:
             end_point_dominated = all([x <= y for x, y in zip(edge.end_point().values(), point.values())])
             return start_point_dominated and end_point_dominated
 
+    class FrontierToEdge:
+
+        """Implements the rules when comparing a frontier to an edge"""
+
+        @staticmethod
+        def dominated(frontier, edge):
+            """Returns True if the frontier is dominated by the edge"""
+            assert isinstance(frontier, FrontierInTwoDimension), "the frontier must be in two-dimensional space"
+            assert isinstance(edge, EdgeInTwoDimension), "the edge must be in two-dimensional space"
+            if frontier.singleton():
+                return DominanceRules.PointToEdge.dominated(frontier.point(), edge)
+            for frontier_edge in frontier.edges():
+                if not DominanceRules.EdgeToEdge.dominated(frontier_edge, edge):
+                    return False
+            return True
+
+    class FrontierToFrontier:
+
+        """Implements the rules when comparing a frontier to another one"""
+
+        @staticmethod
+        def dominated(this, that):
+            """Returns True if 'this' frontier is dominated by 'that' frontier"""
+            assert isinstance(this, FrontierInTwoDimension) and isinstance(that, FrontierInTwoDimension), "the " \
+                "frontiers must be in two-dimensional space"
+            if this.point():
+                return DominanceRules.PointToFrontier.dominated(this.point(), that)
+            for edge in this.edges():
+                if not DominanceRules.EdgeToFrontier.dominated(edge, that):
+                    return False
+            return True
+
     class FrontierToPoint:
 
         """Implements the rules when comparing a frontier to a point"""
@@ -41,17 +103,6 @@ class DominanceRules:
             return DominanceRules.EdgeToPoint.dominated(frontier.edges()[0], point) and \
                 DominanceRules.EdgeToPoint.dominated(frontier.edges()[-1], point)
     
-    class PointToPoint:
-
-        """Implements the rules when comparing a point to another point"""
-
-        @staticmethod
-        def dominated(this, that):
-            """Returns True if 'this' point is dominated by 'that' point, otherwise False"""
-            assert len(this.values()) == len(that.values()), \
-                "the compared points must have the same number of dimensions"
-            return this != that and all([v <= that.values()[i] for i, v in enumerate(this.values())])
-
     class PointToEdge:
 
         """Implements the rules when comparing a point to an edge"""
@@ -76,14 +127,27 @@ class DominanceRules:
             """Returns True if the point is dominated by the frontier, otherwise False"""
             assert isinstance(point, PointInTwoDimension) and isinstance(frontier, FrontierInTwoDimension), \
                 "this method is only available for points and frontiers in two-dimensional space"
-            if point.z2() > frontier.edges()[0].left_point().z2():
+            frontier_north_west_point = frontier.point() or frontier.edges()[0].left_point()
+            if point.z2() > frontier_north_west_point.z2():
                 return False
-            if point.z1() > frontier.edges()[-1].right_point().z1():
+            frontier_south_east_point = frontier.point() or frontier.edges()[-1].right_point()
+            if point.z1() > frontier_south_east_point.z1():
                 return False
             for edge in frontier.edges():
                 if np.dot(point.values(), edge.normal_vector()) >= edge.edge_value():
                     return False
             return True
+
+    class PointToPoint:
+
+        """Implements the rules when comparing a point to another point"""
+
+        @staticmethod
+        def dominated(this, that):
+            """Returns True if 'this' point is dominated by 'that' point, otherwise False"""
+            assert len(this.values()) == len(that.values()), \
+                "the compared points must have the same number of dimensions"
+            return this != that and all([v <= that.values()[i] for i, v in enumerate(this.values())])
 
 
 class ModelBasedDominanceFilter:
--- test/tests/test_dominance.py
+++ test/tests/test_dominance.py
@@ -14,6 +14,40 @@ class TestDominanceRules(TestCase):
     def setUp(self):
         self.assert_that = assert_that
 
+    def test_edge_dominated_by_edge_in_two_dimension(self):
+        """Tests a scenario where an edge is dominated by another edge in two dimension"""
+        this = EdgeInTwoDimension(PointInTwoDimension([3, 6]), PointInTwoDimension([5, 5]))
+        that = EdgeInTwoDimension(PointInTwoDimension([3, 8]), PointInTwoDimension([5, 6]))
+        self.assert_that(DominanceRules.EdgeToEdge.dominated(this, that))
+
+    def test_edge_partially_nondominated_relative_to_edge_in_two_dimension(self):
+        """Tests a scenario where an edge is partially nondominated compared to another edge in two dimension"""
+        this = EdgeInTwoDimension(PointInTwoDimension([5, 5]), PointInTwoDimension([3, 8]))
+        that = EdgeInTwoDimension(PointInTwoDimension([5, 5]), PointInTwoDimension([4, 9]))
+        self.assert_that(not DominanceRules.EdgeToEdge.dominated(this, that))
+
+    def test_edge_dominated_by_frontier_in_two_dimension(self):
+        """Tests a scenario where an edge is dominated by a frontier in two dimension"""
+        edge = EdgeInTwoDimension(PointInTwoDimension([5, 10]), PointInTwoDimension([10, 5]))
+        frontier = FrontierInTwoDimension(point=PointInTwoDimension([11,11]))
+        self.assert_that(DominanceRules.EdgeToFrontier.dominated(edge, frontier))
+        frontier = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([6, 10]), PointInTwoDimension([8, 9])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([8, 9]), PointInTwoDimension([10, 6]))])
+        self.assert_that(DominanceRules.EdgeToFrontier.dominated(edge, frontier))
+
+    def test_edge_partially_nondominated_relative_to_frontier_in_two_dimension(self):
+        """Tests a scenario where an edge is partially nondominated compared to a frontier in two dimension"""
+        edge = EdgeInTwoDimension(PointInTwoDimension([5, 10]), PointInTwoDimension([10, 5]))
+        frontier = FrontierInTwoDimension(point=PointInTwoDimension([7, 7]))
+        self.assert_that(not DominanceRules.EdgeToFrontier.dominated(edge, frontier))
+        frontier = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([1, 9]), PointInTwoDimension([3, 7])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([11, 5]))])
+        self.assert_that(not DominanceRules.EdgeToFrontier.dominated(edge, frontier))
+
     def test_edge_dominated_by_point(self):
         """Tests a scenario where an edge is dominated by a point"""
         point = Point([10, 10, 10])
@@ -22,11 +56,57 @@ class TestDominanceRules(TestCase):
         edge = Edge(Point([5, 10, 10]), Point([10, 5, 10]))
         self.assert_that(DominanceRules.EdgeToPoint.dominated(edge, point))
 
-    def test_edge_nondominated_relative_to_point(self):
-        """Tests a scenario where an edge is relatively nondominated compared to a point"""
+    def test_edge_partially_nondominated_relative_to_point(self):
+        """Tests a scenario where an edge is partially nondominated compared to a point"""
         point = Point([10, 10])
         edge = Edge(Point([5, 11]), Point([8, 8]))
         self.assert_that(not DominanceRules.EdgeToPoint.dominated(edge, point))
+
+    def test_frontier_dominated_by_edge_in_two_dimension(self):
+        """Tests a scenario where a frontier is dominated by an edge in two dimension"""
+        edge = EdgeInTwoDimension(PointInTwoDimension([5, 10]), PointInTwoDimension([10, 5]))
+        frontier = FrontierInTwoDimension(point=PointInTwoDimension([6, 6]))
+        self.assert_that(DominanceRules.FrontierToEdge.dominated(frontier, edge))
+        frontier = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([1, 9]), PointInTwoDimension([3, 7])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([5, 5]))])
+        self.assert_that(DominanceRules.FrontierToEdge.dominated(frontier, edge))
+
+    def test_frontier_partially_nondominated_relative_to_edge_in_two_dimension(self):
+        """Tests a scenario where a frontier is partially nondominated compared to an edge in two dimension"""
+        edge = EdgeInTwoDimension(PointInTwoDimension([5, 10]), PointInTwoDimension([10, 5]))
+        frontier = FrontierInTwoDimension(point=PointInTwoDimension([8, 8]))
+        self.assert_that(not DominanceRules.FrontierToEdge.dominated(frontier, edge))
+        frontier = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([1, 9]), PointInTwoDimension([3, 7])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([11, 5]))])
+        self.assert_that(not DominanceRules.FrontierToEdge.dominated(frontier, edge))
+
+    def test_frontier_dominated_by_frontier_in_two_dimension(self):
+        """Tests a scenario where a frontier is dominated by another frontier in two dimension"""
+        this = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([1, 9]), PointInTwoDimension([3, 7])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([5, 5]))])
+        that = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([2, 9]), PointInTwoDimension([3, 8])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 8]), PointInTwoDimension([5, 6]))])
+        self.assert_that(DominanceRules.FrontierToFrontier.dominated(this, that))
+
+    def test_frontier_partially_nondominated_relative_to_frontier_in_two_dimension(self):
+        """Tests a scenario where a frontier is partially nondominated compared to another frontier in two dimension"""
+        this = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([1, 9]), PointInTwoDimension([3, 7])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([5, 5]))])
+        that = FrontierInTwoDimension(
+            edges=[
+                FrontierEdgeInTwoDimension(PointInTwoDimension([2, 9]), PointInTwoDimension([3, 7])), 
+                FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([5, 6]))])
+        self.assert_that(not DominanceRules.FrontierToFrontier.dominated(this, that))
 
     def test_frontier_dominated_by_point_in_two_dimension(self):
         """Tests a scenario where a frontier is dominated by a point in two dimension"""
@@ -39,8 +119,8 @@ class TestDominanceRules(TestCase):
                 FrontierEdgeInTwoDimension(PointInTwoDimension([3, 7]), PointInTwoDimension([5, 5]))])
         self.assert_that(DominanceRules.FrontierToPoint.dominated(frontier, point))
 
-    def test_frontier_nondominated_relative_to_point_in_two_dimension(self):
-        """Tests a scenario where a frontier is relatively nondominated compared to a point in two dimension"""
+    def test_frontier_partially_nondominated_relative_to_point_in_two_dimension(self):
+        """Tests a scenario where a frontier is partially nondominated compared to a point in two dimension"""
         point = Point([10, 10])
         frontier = FrontierInTwoDimension(point=PointInTwoDimension([10, 10]))
         self.assert_that(not DominanceRules.FrontierToPoint.dominated(frontier, point))
@@ -95,6 +175,20 @@ class TestDominanceRules(TestCase):
         self.assert_that(not DominanceRules.PointToFrontier.dominated(point, frontier))
         point = PointInTwoDimension([9, 5])
         self.assert_that(not DominanceRules.PointToFrontier.dominated(point, frontier))
+
+    def test_point_dominated_by_point(self):
+        """Tests a scenario where a point is dominated by another point"""
+        base_point = Point([10, 10, 10])
+        compared_point = Point([10, 11, 10])
+        self.assert_that(DominanceRules.PointToPoint.dominated(base_point, compared_point))
+
+    def test_point_nondominated_relative_to_point(self):
+        """Tests a scenario where a point is relatively nondominated compared to another point"""
+        base_point = Point([10, 10, 10])
+        compared_point = Point([10, 10, 10])
+        self.assert_that(not DominanceRules.PointToPoint.dominated(base_point, compared_point))
+        compared_point = Point([11, 10, 9])
+        self.assert_that(not DominanceRules.PointToPoint.dominated(base_point, compared_point))
 
 
 if __name__ == '__main__':
